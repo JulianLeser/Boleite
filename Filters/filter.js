f1c2dbe06faaf1aadc3ef2e6c61b78496f8e6eaf
Julian Leser <leser.julian.noah@gmail.com>
2022-08-01 15:05:08 +0200

    Update filter.js

--- Filters/filter.js
+++ Filters/filter.js
@@ -1,5 +1,5 @@
 usableData.items.forEach(item => {console.log(item.snippet.description);
-	if(!item.snippet.description.includes('Obama') ){
+	if(!item.snippet.description.includes('No videos that include this string in their description will be shown') ){
 		items.push(item);
 	}
 });
